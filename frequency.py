e6345d21f3e3fe95916c2191453a9c9b11ffcd9f
Ka Hung Wong <richard.wkh@gmail.com>
2017-12-20 13:05:59 -0800

    To calculate the relative frequency of the words in tweets.

---
+++ frequency.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 19 12:03:46 2017
+
+@author: richa
+"""
+
+import sys
+import json 
+from collections import Counter
+
+def get_text(tweet):
+    return json.loads(tweet, encoding = 'utf-8').get('text', '')
+
+def get_relative_freq(texts):
+    # generate the freq
+    freq = Counter(word for text in texts for word in text)
+    # total occurrences
+    sum_freq = sum( freq.values() )
+    return {word: freq[word]/float(sum_freq) for word in freq}
+    
+def main():
+    tweet_file = sys.argv[1]
+    
+    with open(tweet_file, 'r') as f:
+        texts = [get_text(line).split() for line in f]
+    
+    # generate the relative freq
+    rel_freq = get_relative_freq(texts)
+    
+    for word in rel_freq:
+        sys.stdout.write(
+            '{0} {1:f}\n'.format(word.encode('utf-8'), rel_freq[word])
+                        ) 
+    
+if __name__ == '__main__':
+    main()
+    
