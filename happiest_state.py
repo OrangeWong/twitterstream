477a96e2aa6a73d115b78f0a20a21c3eb97a2cc4
Ka Hung Wong <richard.wkh@gmail.com>
2017-12-21 10:59:33 -0800

    style: add more comments on the functions.

--- happiest_state.py
+++ happiest_state.py
@@ -11,23 +11,59 @@ import operator
 from collections import defaultdict
 
 def get_text(tweet):
+    '''
+    Args:
+        tweet (str): A string that contains a tweet's text.
+    
+    Returns:
+        str: The string of text in the tweet object or an empty string if 
+        text section is empty. 
+    '''
     return json.loads(tweet, encoding = 'utf-8').get('text', '')
 
 def get_sentiment(tweet_text, scores):
-    # to caluclate the sentiment for a tweet
+    '''
+    Args:
+        tweet_text (str): A string that contains the text in tweet.
+        scores (dict): A dict of English word scores.
+    Returns:
+        float: The sentiment of the tweet text. 
+    '''
+    
+    # Zero if word is not in the scores
     sentiment = sum( scores.get(word, 0) for word in tweet_text.split(" ") )
+    
     return sentiment
     
 def get_afinn_scores(afinnfile):
-    scores = {} # initialize an empty dictionary
+    '''
+    Args:
+        afinnfile (file): A file contains the English word scores, and the file
+        is tab-delimited.
+        
+    Returns:
+        dict: The dict that contains pairs of English word and the related 
+        score
+    '''
+    # initialize an empty dictionary
+    scores = {}
     with open(afinnfile, 'r') as f:
         for line in f:
-            term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
-            scores[term] = int(score)  # Convert the score to an integer.
+            # The file is tab-delimited.
+            term, score  = line.split("\t")  
+            scores[term] = int(score)
     return scores
 
 def get_state(tweet):
-    # if the tweet is located, info is in the place dictionary
+    '''
+    Args:
+        tweet (string): A string contains the tweet's object
+        
+    Returns:
+        Str: The state in the US if it is written in tweet's place, otherwise
+        None.
+    '''
+    # A list of states in the US
     states = {
         'AK': 'Alaska',
         'AL': 'Alabama',
@@ -88,9 +124,11 @@ def get_state(tweet):
         'WY': 'Wyoming'}
     
     try:
+        # try if the tweet contains place
         place = json.loads(tweet, encoding = 'utf-8').get('place')
         _, state=(x.strip().upper() for x in place.get('full_name').split(","))
-        if state in states: 
+        if state in states:
+            # check if state is in states
             return state
         else:
             return None
@@ -100,22 +138,28 @@ def get_state(tweet):
 def main():
     sent_file = sys.argv[1]
     tweet_file = sys.argv[2]
+    
     scores = get_afinn_scores(sent_file)
     
     with open(tweet_file, 'r') as f:
+        # get a list of tuples containing state and score of the tweets
         US_scores = [ (get_state(line), get_sentiment(get_text(line), scores)) 
                      for line in f if get_state(line)]
     
+    # initiate two defaultdicts
     states_score, states_count = defaultdict(int), defaultdict(int)
     for key, val in US_scores:
         states_score[key] += val
         states_count[key] += 1
-        
+    
+    # return a dict contains state and its average score
     states_avg = {state: states_score[state]/float(states_count[state])
         for state in states_score}
     
-    # stdout the sentiment
+    # get the max based on the average score
     happiest_state = max(states_avg.iteritems(), key=operator.itemgetter(1))[0]
+    
+    # stdout the sentiment
     sys.stdout.write('{0} {1:f}\n'.format(happiest_state.encode('utf-8'), 
                                           states_avg[happiest_state]))
     
