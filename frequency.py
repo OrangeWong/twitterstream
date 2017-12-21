79258ca61ad33d30b961a2123d41fdc00856b9d8
Ka Hung Wong <richard.wkh@gmail.com>
2017-12-21 11:04:41 -0800

    style: add more comments on the functions in the file.

--- frequency.py
+++ frequency.py
@@ -10,13 +10,30 @@ import json
 from collections import Counter
 
 def get_text(tweet):
+    '''
+    Args:
+        tweet (str): A string that contains a tweet object.
+    
+    Returns:
+        str: The string of text in the tweet object or an empty string if 
+                text section is empty. 
+    '''
     return json.loads(tweet, encoding = 'utf-8').get('text', '')
 
 def get_relative_freq(texts):
-    # generate the freq
+    '''
+    Args:
+        texts (list): A list contains all the words in the tweets.
+        
+    Returns:
+        dict: A dict contains the relative frequency on word.
+    '''
+    
+    # generate the freq to store the counting of word in texts
     freq = Counter(word for text in texts for word in text)
     # total occurrences
     sum_freq = sum( freq.values() )
+    
     return {word: freq[word]/float(sum_freq) for word in freq}
     
 def main():
@@ -28,6 +45,7 @@ def main():
     # generate the relative freq
     rel_freq = get_relative_freq(texts)
     
+    # stdout the pairs of word and its relative frequency
     for word in rel_freq:
         sys.stdout.write(
             '{0} {1:f}\n'.format(word.encode('utf-8'), rel_freq[word])
